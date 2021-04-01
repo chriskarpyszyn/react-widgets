725ab763c1e3c65e70428cf7a9bea2c60a73ff07
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-04-01 09:33:50 -0400

    feat: hook up useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
 	const [term, setTerm] = useState("");
+
+	useEffect(() => {
+		//make request to wikipedia api
+	}, [term]);
 
 	return (
 		<div>
