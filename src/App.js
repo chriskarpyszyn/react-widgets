88bd05cae739876414221af7a1ba1943dec35d7a
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-05-16 16:35:45 -0400

    feat: scaffold new translation component
    hide other components for now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
 	{
@@ -36,23 +37,26 @@ const options = [
 ];
 
 export default () => {
-	const [selected, setSelected] = useState(options[0]);
-	const [showDropdown, setShowDropdown] = useState(true);
+	// const [selected, setSelected] = useState(options[0]);
+	// const [showDropdown, setShowDropdown] = useState(true);
 
 	return (
 		<div>
+			<Translate />
+
+			{/*hide for now*/}
 			{/*<Accordion items={items} />*/}
 			{/*<Search />*/}
-			<button onClick={() => setShowDropdown(!showDropdown)}>
-				Toggle Dropdown
-			</button>
-			{showDropdown ? (
-				<Dropdown
-					selected={selected}
-					onSelectedChange={setSelected}
-					options={options}
-				/>
-			) : null}
+			{/*<button onClick={() => setShowDropdown(!showDropdown)}>*/}
+			{/*	Toggle Dropdown*/}
+			{/*</button>*/}
+			{/*{showDropdown ? (*/}
+			{/*	<Dropdown*/}
+			{/*		selected={selected}*/}
+			{/*		onSelectedChange={setSelected}*/}
+			{/*		options={options}*/}
+			{/*	/>*/}
+			{/*) : null}*/}
 		</div>
 	);
 };
---
+++ src/components/Translate.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+
+const options = [
+	{
+		label: "French",
+		value: "fr",
+	},
+	{
+		label: "Russian",
+		value: "ru",
+	},
+	{
+		label: "Chinese",
+		value: "cn",
+	},
+];
+
+const Translate = () => {
+	const [language, setLanguage] = useState(options[0]);
+
+	return (
+		<div>
+			<Dropdown
+				selected={language}
+				onSelectedChange={setLanguage}
+				options={options}
+			/>
+		</div>
+	);
+};
+
+export default Translate;
